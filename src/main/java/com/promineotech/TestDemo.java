665d5c2eeac51d67890a083fc7f78fba826ebe0e
unknown <ifireman310@gmail.com>
2024-10-22 21:15:25 -0700

    Completed the rest of the steps in the weekly assignment

--- src/main/java/com/promineotech/TestDemo.java
+++ src/main/java/com/promineotech/TestDemo.java
@@ -1,7 +1,13 @@
 package com.promineotech;
 
+import java.util.Random;
+
 public class TestDemo {
 
+	
+	/* STEP 1 & 2
+	 * Adds two positive integers together, throws illegal argument exception otherwise
+	 */
 	
 	public int addPositive(int a, int b) throws IllegalArgumentException {
 		if (a > 0 && b > 0) {
@@ -10,4 +16,38 @@ public class TestDemo {
 			throw new IllegalArgumentException("Both parameters must be positive!");
 		}	
 	};
+	
+	
+	/* STEP 3
+	 * This method is meant to multiply two negative integers
+	 * If a 0 or positive number is input, we expect the method to throw an illegal 
+	 * argument exception
+	 */
+	
+	public double multiplyNegatives(int a, int b) throws IllegalArgumentException {
+		
+		if (a < 0 && b < 0) {
+			return a*b;
+		} else {
+			throw new IllegalArgumentException("Both parameters must be negative!");
+		}
+		
+	}
+	
+	
+	/* STEP 4-1
+	 * Gets a random integer from 1-10 and returns that integer squared
+	 * 
+	 */
+	
+	public int randomNumberSquared() {
+		int random_integer = getRandomInt();
+		return random_integer*random_integer;
+	}
+	
+	public int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	}
+	
 }
--- src/test/java/com/promineotech/TestDemoJUnitTest.java
+++ src/test/java/com/promineotech/TestDemoJUnitTest.java
@@ -3,6 +3,8 @@ package com.promineotech;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 import java.util.stream.Stream;
 
@@ -41,5 +43,44 @@ class TestDemoJUnitTest {
 			Arguments.of(-100,-300,0,true)
 		);
 		// @formatter: on
+	}
+	
+	
+	/* STEP 2
+	 * 
+	 */
+	
+	@Test
+	void assertThatPairsOfPositiveNumbersAreAddedCorrectly() {
+		assertThat(testDemo.addPositive(4,5)).isEqualTo(9);
+		assertThat(testDemo.addPositive(40,50)).isEqualTo(90);
+		assertThat(testDemo.addPositive(1,1)).isEqualTo(2);
+	}
+	
+	/* STEP 3
+	 * This test is meant to test two negative integer numbers being multiplied together
+	 * If a 0 or positive number is input, we expect the method to throw an illegal 
+	 * argument exception
+	 */
+	
+	@Test
+	void assertThatPairsOfNegativeNumbersAreMultipliedCorrectly() {
+		assertThat(testDemo.multiplyNegatives(-1, -1)).isEqualTo(1);
+		assertThat(testDemo.multiplyNegatives(-9, -9)).isEqualTo(81);
+		assertThatThrownBy(() -> 
+		testDemo.multiplyNegatives(5, 2)).isInstanceOf(IllegalArgumentException.class);
+	}
+	
+	/* STEP 4
+	 * 
+	 * 
+	 */
+	
+	@Test
+	void assertThatNumberSquaredIsCorrect() {
+		TestDemo mockDemo = spy(testDemo);
+		doReturn(5).when(mockDemo).getRandomInt();
+		int fiveSquared = mockDemo.randomNumberSquared();
+		assertThat(fiveSquared).isEqualTo(25);
 	}
 }
